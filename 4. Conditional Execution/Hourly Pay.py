689068697f1dfc035e65a888c8aeaa1fdcd1c910
tudor.marica <tudor.code@gmail.com>
2024-06-07 14:41:30 +0300

    Update Hourly Pay.py

--- 4. Conditional Execution/Hourly Pay.py
+++ 4. Conditional Execution/Hourly Pay.py
@@ -1,1 +1,19 @@
+# Hourly Pay
 
+# calculate Hourly pay including overtime cases 
+
+hours = int(input("Enter number of hours worked: "))
+rate = int(input("Enter rate per hour: "))
+
+pay_regular = 40 * float(rate)
+
+
+if hours > 40: 
+  print("Overtime")
+  hours_overtime = hours - 40
+  rate_overtime = float(rate) + float(rate) * 0.5 
+  pay_overtime_bonus = hours_overtime * rate_overtime
+  pay = pay_regular + pay_overtime_bonus
+  print(pay, pay_overtime_bonus)
+else:
+  print("Regular", pay_regular)
