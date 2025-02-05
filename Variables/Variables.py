b992adbf8572f288340c854bcdbd3ca2a2985bb8
tudor.marica <tudor.code@gmail.com>
2025-02-05 18:37:57 +0200

    Update Variables.py

--- Variables/Variables.py
+++ Variables/Variables.py
@@ -1,1 +1,9 @@
 #Write a program to prompt the user for hours and rate per hour to compute gross pay.
+
+
+hours_worked = int(input("Please enter your hours "))
+hourly_rate = int(input("Please enter your rate "))
+
+gross_pay = hours_worked * hourly_rate
+
+print("Gross pay:", gross_pay)
