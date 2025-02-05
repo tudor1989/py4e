9a9aa498f962deb79dc7c42dbfdfe2edcf05bdb5
tudor.marica <tudor.code@gmail.com>
2025-02-05 18:41:06 +0200

    Create Hourly Pay Try & Except.py

---
+++ 4. Conditional Execution/Hourly Pay Try & Except.py
@@ -0,0 +1,1 @@
+# Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program. The following shows two executions of the program:
