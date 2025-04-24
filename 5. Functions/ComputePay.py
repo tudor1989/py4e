433a9eae6dec6b446a8379418aefd9626e167f73
tudor.marica <tudor.code@gmail.com>
2025-04-24 14:09:08 +0300

    Update ComputePay.py

--- 5. Functions/ComputePay.py
+++ 5. Functions/ComputePay.py
@@ -1,1 +1,18 @@
 # rewrite your pay computation with time-and-a-half for overtime and create a function called computepay which takes two parameters(hours and rate)
+
+def ComputePay(hours, rate):
+  """Function to calculate regular pay and overtime bonus"""
+  pay_regular = 40 * float(rate)
+
+
+  if hours > 40: 
+    
+    hours_overtime = hours - 40
+    rate_overtime = float(rate) + float(rate) * 0.5 
+    pay_overtime_bonus = hours_overtime * rate_overtime
+    pay_overtime = pay_regular + pay_overtime_bonus
+    
+    return f"Pay with overtime bonus {pay_overtime}"
+
+  else:
+    return f"Regular pay {pay_regular}"
