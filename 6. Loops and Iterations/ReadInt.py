31f3556c7659d89eade50888396417a6969de6d0
tudor.marica <tudor.code@gmail.com>
2025-08-12 19:05:20 +0300

    Update ReadInt.py

--- 6. Loops and Iterations/ReadInt.py
+++ 6. Loops and Iterations/ReadInt.py
@@ -1,1 +1,34 @@
+def main():
+    
+  num_list = []
+  while True:
 
+    num = input("Enter a value: ")
+
+    if num == 'done':
+      break
+    
+    try:
+      int(num) == True
+    
+    except ValueError:
+      print("Please enter a number!")
+    
+    else:
+      num_list.append(int(num))
+
+
+  #print(num_list)
+
+
+  sum = 0
+  count = 0
+  avg = 0
+
+  for elem in num_list:
+    sum += elem
+    count += 1
+
+  print("Sum of elements is: ", sum)
+  print("Count of elements is: ", count)
+  print("Avg of elements is: ", (sum/count))
